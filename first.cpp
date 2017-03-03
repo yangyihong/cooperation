3e847deb967f28d33ff30c581692c57444e25d65
yangkailun <2362080354@qq.com>
2017-03-03 10:30:42 +0800

    最初版本

---
+++ first.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//大家要注意模块化开放，互相之间要协作。 
+
+//判断字符串长度是否大于5的方法，如果大于5返回true,否则返回false
+bool isGreaterFive(String a){
+    //杨已鸿编写
+     
+}
+//输出函数，如果字符串的长度大于5，输出最后一个字符，如果字符串的长度小于等于5，输出第一个字符 
+void outPut(String a){
+    //王艳丽编写 
+} 
+int main(){
+    String a;
+    cin>>a;
+    
+    isGreaterFive("123456");
+    isGreaterFive("0123");  
+    
+    outPut("123456");
+    outPut("0123");
+    
+    return 0;
+} 
