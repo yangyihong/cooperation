72ae9d250d24b2fc399a263fbc1dacc4d7a8bed9
yangyihong <1695277539@qq.com>
2017-03-03 11:50:22 +0800

    实现比较功能

--- first.cpp
+++ first.cpp
@@ -1,26 +1,31 @@
 #include<iostream>
+#include"string.h"
 using namespace std;
 
-//大家要注意模块化开放，互相之间要协作。 
+//大家要注意模块化开放，互相之间要协作。
 
 //判断字符串长度是否大于5的方法，如果大于5返回true,否则返回false
-bool isGreaterFive(String a){
+bool isGreaterFive(string a){
     //杨已鸿编写
-     
+   int t= a.length();
+   if(t>5) return true;
+   else return false;
+
 }
-//输出函数，如果字符串的长度大于5，输出最后一个字符，如果字符串的长度小于等于5，输出第一个字符 
+//输出函数，如果字符串的长度大于5，输出最后一个字符，如果字符串的长度小于等于5，输出第一个字符
 void outPut(String a){
-    //王艳丽编写 
-} 
+    //王艳丽编写
+}
 int main(){
     String a;
     cin>>a;
-    
-    isGreaterFive("123456");
-    isGreaterFive("0123");  
-    
+
+  isGreaterFive("123456");
+  isGreaterFive("0123");
+
     outPut("123456");
     outPut("0123");
-    
+
+
     return 0;
-} 
+}
